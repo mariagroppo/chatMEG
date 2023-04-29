e103935681f27e275713d67d4a80ebaf3967f778
Mey Groppo <mariagroppo86@gmail.com>
2023-04-29 20:32:51 -0300

    Cambios para cargar en glitch

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import router from './src/routes/chat.js';
 import __dirname from './utils.js';
 
 const app=express();
-const PORT=8080;
+const PORT=process.env.PORT||8080;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/src/public'));
